ed006b3f8c110e5a04922b059daf608032c5b23b
s <sangjp.22@gmail.com>
2024-10-08 00:27:56 +0900

    Initial commit

---
+++ lesson3/js/test.js
@@ -0,0 +1,14 @@
+function doIt(){
+    num = 10;
+    if(num == 10){
+        alert("10desu");
+    }else if(num == 20){
+        alert("20desu");
+    }else{
+        alert("different")
+    }
+   }
+   function doIt2(){
+    for(i=1;i<=3;i++)
+    alert(i)
+   }
